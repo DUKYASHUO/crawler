e6840504005b204a176b7f28262ce5a157f6757f
DUKAYASHUO <duke.gz.zip@gmail.com>
2017-06-15 21:46:37 +0800

    Add play ws demo in project.

--- app/controllers/CtripController.scala
+++ app/controllers/CtripController.scala
@@ -1,12 +1,16 @@
 package controllers
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import javax.inject.Inject
 import daos.params.CtripDao.Create
 import org.joda.time.DateTime
+import play.api.libs.ws.{WSClient, WSRequest, WSResponse}
 import play.api.mvc._
 import services.CtripService
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
 
-class CtripController @Inject()(ctripService: CtripService) extends Controller {
+class CtripController @Inject()(ctripService: CtripService, ws: WSClient) extends Controller {
 
   def create() = Action {
     val id = ctripService.create(
@@ -20,5 +24,23 @@ class CtripController @Inject()(ctripService: CtripService) extends Controller {
       )
     )
     Ok("Got it : " + id)
+  }
+
+  def ping() = Action {
+    val request: WSRequest = ws.url("http://www.baidu.com")
+
+    val complexRequest: WSRequest =
+      request.withHeaders("Accept" -> "application/json")
+
+    val futureResponse: Future[WSResponse] = complexRequest.get()
+
+    futureResponse.onSuccess {
+      case result => {
+        println(result.body)
+      }
+    }
+
+    val result = Await.result(futureResponse, Duration(100, "millis")).allHeaders
+    Ok("Got it ." + result)
   }
 }
--- build.sbt
+++ build.sbt
@@ -10,10 +10,11 @@ libraryDependencies ++= Seq(
   jdbc,
   cache,
   ws,
-  "org.scalatestplus.play" %% "scalatestplus-play"    % "1.5.1" % Test,
-  "mysql" % "mysql-connector-java"                    % "5.1.39",
-  "org.scalikejdbc" %% "scalikejdbc"                  % "3.0.0",
-  "org.scalikejdbc" %% "scalikejdbc-config"           % "3.0.0",
-  "org.scalikejdbc" % "scalikejdbc-play-initializer_2.10" % "2.4.5"
+  "org.scalatestplus.play" %% "scalatestplus-play"         % "1.5.1" % Test,
+  "mysql" % "mysql-connector-java"                         % "5.1.39",
+  "org.scalikejdbc" %% "scalikejdbc"                       % "3.0.0",
+  "org.scalikejdbc" %% "scalikejdbc-config"                % "3.0.0",
+  "org.scalikejdbc" % "scalikejdbc-play-initializer_2.10"  % "2.4.5",
+  "net.ruippeixotog" %% "scala-scraper"                    % "2.0.0-RC2"
 )
 
